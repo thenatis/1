a8c15ca2748e86e727d42ae38aa61ddb2f9fb924
William <wbraffman@gmail.com>
2019-06-17 16:28:20 -0400

    init

---
+++ pages/history-of-ios/history-of-ios.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-history-of-ios',
+  templateUrl: 'history-of-ios.html'
+})
+export class HistoryOfIOSPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
---
+++ pages/scenic/scenic.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-scenic',
+  templateUrl: 'scenic.html'
+})
+export class ScenicPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
